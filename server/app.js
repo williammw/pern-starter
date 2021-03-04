8c1ca4d8c6d49d92e74470a7d1164c550f72736c
wmwwong <wmwwong@legco.gov.hk>
2021-03-04 09:59:32 +0800

    ini

--- server/app.js
+++ server/app.js
@@ -1,27 +1,34 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-//
-const db = require('./config/db')
+// Database
+const db = require('./config/db');
 
-
-//Test db
+// Test DB
 db.authenticate()
-  .then(() => console.log('db connected'))
-  .catch(err => console.log(err))
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
 
+const app = express();
 
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-const app = express()
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
 
-
+// Gig routes
+app.use('/gigs', require('./routes/gigs'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('server is running on port5000'));
-
-
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
